61c392d45dee371afd0acef88fbe846cef548cb4
s246127 <hiroki71027923@icloud.com>
2020-12-30 19:09:36 +0900

    ログインしているユーザーのみ商品ページへ遷移routing error

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,28 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
+
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render_new
+    end
+  end
 end
+
+
+private
+
+def item_params
+  params.require(:item).permit(:name, :explain, :price, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_date_id)
+end 
