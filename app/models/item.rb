39c0e2084ebe21da22cdc4c9187d9726dfc544f0
s246127 <hiroki71027923@icloud.com>
2021-01-04 11:11:44 +0900

    商品出品機能の細かな修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,12 @@
 class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to: [:category, :condition, :postage, :prefecture, :shipping_date]
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
 
   has_one :purchase
   belongs_to :user
