524ac9936b20d5170e9a07a9f2b7e61c5a52b7c2
s246127 <hiroki71027923@icloud.com>
2021-01-07 20:34:25 +0900

    購入画面に遷移する

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,20 +3,23 @@ class Order
   attr_accessor :user_id, :item_id, :token, :postal_number, :prefecture_id, :city, :district, :building_number, :phone_number
 
   # ここにバリデーションの処理を書く
-  with_options presence :trure do
+  with_options presence: true do
     validates :postal_number, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
-    validates :phone_number, format: { with: /\A0[0-9]+\z/, message: 'number is invalid. Include half-width numbers' } 
     validates :city
     validates :district
+    validates :user_id
+    validates :item_id
+    validates :phone_number, format: { with: /\A0[0-9]+\z/, message: 'number is invalid. Include half-width numbers' } 
   end
+  
 
   def save
     # 各テーブルにデータを保存する処理を書く 
     # purchasesテーブルにクレカの情報を保存
-    Purchase.create(user_id: user.id, item_id: item.id)
+    purchase = Purchase.create(user_id: user.id, item_id: item.id)
 
     # addressテーブルに住所の情報を保存
-    Address.create(postal_number: postal_number, prefecture_id: prefecture_id, city: city, district: district, building_number: building_number, phone_number: phone_number)
+    Address.create(postal_number: postal_number, prefecture_id: prefecture_id, city: city, district: district, building_number: building_number, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
