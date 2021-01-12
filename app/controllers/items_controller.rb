fd6dcb4639f07b61ef0b6e528a41826ea694d1ec
s246127 <hiroki71027923@icloud.com>
2021-01-12 21:29:04 +0900

    Basic認証を導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user_id || if @item.purchase.present?
+    if current_user.id != @item.user_id || @item.purchase.present?
       redirect_to root_path 
     end
   end
